1d5c3927bba0bb94ad02ed5409f485caffbb348f
Jude <boblitejude@gmail.com>
2018-11-14 20:02:01 +0200

    login UI with showpassword

--- app/src/main/java/com/example/bobby/nwumobile/MainActivity.java
+++ app/src/main/java/com/example/bobby/nwumobile/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.bobby.nwumobile;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
-
-
+    private EditText nwunumber;
+    private EditText password;
+    private Button login;
+    private CheckBox showpassword;
 
 
 
@@ -13,5 +24,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Drawable drawable = getResources().getDrawable(R.drawable.background);
+        drawable.setAlpha(50);
+
+        nwunumber = findViewById(R.id.StudentNo);
+        password = findViewById(R.id.Password);
+        login = findViewById(R.id.button);
+        showpassword = findViewById(R.id.show_hide);
+
+        CompoundButton.OnCheckedChangeListener changeListener = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    showpassword.setText(R.string.hide_password);
+                    password.setInputType(InputType.TYPE_CLASS_TEXT);
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+                else {
+                    showpassword.setText(R.string.show_password);
+                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        };
+        showpassword.setOnCheckedChangeListener(changeListener);
+
+
+
+
     }
 }
